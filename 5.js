8f36051f12859104cb19c11fb97796eab54f9f1e
ImPoemy <honey2002111517@gmail.com>
2025-08-13 22:47:16 +0800

    Delete 5.js

--- 5.js
+++
@@ -1,4 +0,0 @@
-fetch("https://script.google.com/macros/s/AKfycbxDsVPA5JATA7NkmuC3XokBa2gedWE_ffaLElMUHPRllHUTr4JzzwJrstFanpnwmtHD/exec", {
-  method: "POST",
-  body: JSON.stringify({ player, result })
-});
